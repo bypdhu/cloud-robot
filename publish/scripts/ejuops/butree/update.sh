e45b01ba72494340281bd009fc2c3d0202270eb7
Administrator <bian@test>
2017-07-11 16:06:34 +0800

    13

--- publish/scripts/ejuops/butree/update.sh
+++ publish/scripts/ejuops/butree/update.sh
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash -
+### Auth: Jin
+### Date: 2016-11-07
 
-filepn=$1
-
-echo "the file path and name is: $filepn"
+if [ $# -eq 2 ];then
+    /usr/bin/unzip -o  $1  -d /data/www/butree.ejuops.com && /bin/cp /data/pkgs/.env  /data/www/butree.ejuops.com/
+    test $? -eq 0 && printf "Update ok" || printf "Update fail"
+    chmod -R 777 /data/www/butree.ejuops.com/storage/
+    chmod -R 777 /data/www/butree.ejuops.com/vendor/
+    chmod -R 777 /data/www/butree.ejuops.com/bootstrap/cache/
+else
+    printf "Give two args, Usge: $0 sitename filename\n"
+fi
